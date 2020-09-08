af8c1e8017a4dbdff9050e9c6b7fb09fd7ec23a8
Keith Bartley <bartley.keith@gmail.com>
2020-09-08 13:48:43 +0100

    greeting change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=> {
-    res.send("Welcome to the homepage!!");
+    res.send("I am a NODE.JS Server :) !!");
 })
 
 const port = process.env.port || 3000;
